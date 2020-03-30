ad83b889769fb89681cff0709410a2f6eafa0104
Pierre Wacrenier <pierre@wacrenier.me>
2020-03-29 20:01:58 -0700

    Remove long description

--- setup.py
+++ setup.py
@@ -7,17 +7,12 @@ def get_requirements():
     except:
         pass
 
-def readme_content():
-    with open('README.md') as handle:
-        return handle.read()
-
 setup(
     name="email-lib",
     version="0.1.0",
     author="Pierre Wacrenier",
     author_email="pierre@wacrenier.me",
     description="Simplify email organization",
-    long_description=readme_content(),
     url="https://github.com/mota/email-lib",
     install_requires=get_requirements(),
     packages=find_packages(),
